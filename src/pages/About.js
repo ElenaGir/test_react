0e1e6ee90ed8ae07a2007fc351253b985bd81fe2
ElenaGir <elena.girard@gmail.com>
2022-10-18 13:56:39 +0200

    first route

---
+++ src/pages/About.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Cumque sequi incidunt reprehenderit vitae sapiente!
+                Corporis officia quaerat, aspernatur excepturi rerum architecto ea iusto, earum voluptatibus quibusdam voluptatem accusamus aliquam eaque obcaecati consequuntur at omnis soluta tempore saepe impedit veritatis neque. Nesciunt vel exercitationem ipsum quo, repellendus ea dicta harum et earum fugit id repellat rerum at omnis,
+                voluptatem iusto amet dolorum dolore architecto.
+                Soluta earum quis pariatur tempora placeat sapiente impedit ab tenetur facere ut! Quam, quod minima. Placeat nihil modi doloremque repellendus quasi quo, nemo molestiae perspiciatis quam, delectus fuga laudantium blanditiis deleniti, totam sit assumenda consectetur repellat sed.
+            </p>
+            <br />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                Architecto quia hic nisi officia quis, ipsa minus numquam, commodi iure sed id provident
+                 eligendi quibusdam maiores, accusantium incidunt! Vero modi repellendus saepe laborum, aperiam 
+                 consequuntur ullam? Accusantium possimus atque id pariatur animi doloremque impedit delectus cum!
+                  Consequuntur ducimus harum velit dolorum ex sunt. Amet quis voluptates aut reprehenderit 
+                  dignissimos alias quibusdam! Reiciendis id tenetur quaerat atque commodi expedita corrupti cumque 
+                  molestias suscipit eveniet eligendi esse, maxime minus, sapiente excepturi nisi adipisci magni? 
+                  Quae blanditiis dignissimos quidem, molestias temporibus cumque magnam exercitationem minus 
+                  dolore ullam at obcaecati iure id doloremque ducimus repellat voluptatem ipsum ab reiciendis 
+                  sunt! Harum repellat modi officia non nihil eaque rem tempore laborum? Porro veniam dolorem, 
+                  provident consectetur magni aut, unde totam minima natus excepturi alias pariatur earum. 
+                  Voluptatibus, quia facere iusto delectus numquam quisquam velit praesentium sit eveniet 
+                  veritatis veniam provident hic incidunt doloremque tenetur qui voluptatum voluptatem unde 
+                  ducimus repudiandae quidem magnam fugiat cupiditate pariatur! Commodi recusandae veritatis, 
+                  aliquam vel eos laboriosam non amet. Est ipsa, aliquid sint dolores magni cum mollitia laborum a,
+                   earum ea omnis id minus nihil exercitationem iure et distinctio consequatur laboriosam dicta 
+                   quam autem illum unde. Autem possimus quod perferendis voluptates!
+
+            </p>
+        </div>
+    );
+};
+
+export default About;
