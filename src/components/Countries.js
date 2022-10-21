5fcb96ec810b08e91a422eed7440cfd3f7b73581
ElenaGir <elena.girard@gmail.com>
2022-10-21 18:44:54 +0200

    add input range

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -4,6 +4,10 @@ import Card from './Card';
 
 const Countries = () => {
     const [data, setData] = useState([]);
+    //const for range
+    const [rangeValue, setRangeValue] = useState(36);
+
+    //useEffect se joue lorsque le composant est monté
     useEffect(() => {
         axios.get("https://restcountries.com/v3.1/all")
             .then((res) => setData(res.data));
@@ -11,9 +15,13 @@ const Countries = () => {
     }, [])
     return (
         <div className='countries'>
-            <h1>COUNTRIES</h1>
+            <ul className="radio-container">
+                <input type="range" min="1" max="250" defaultValue={rangeValue}
+                onChange={(e) => setRangeValue(e.target.value)}
+                />
+            </ul>
             <ul>
-                {data.map((country, index) => ( 
+                {data.slice(0, rangeValue).map((country, index) => ( 
                 <Card key={index} country={country}/>
                 ))}
             </ul>
