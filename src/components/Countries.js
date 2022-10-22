50bc16b3f9bfedcf8440db11bfe816871b7f33fd
ElenaGir <elena.girard@gmail.com>
2022-10-22 12:53:05 +0200

    add button radio continents

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -6,6 +6,8 @@ const Countries = () => {
     const [data, setData] = useState([]);
     //const for range
     const [rangeValue, setRangeValue] = useState(36);
+    const [selectedRadio, setSelectedRadio] = useState("");
+    const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
 
     //useEffect se joue lorsque le composant est monté
     useEffect(() => {
@@ -17,12 +19,24 @@ const Countries = () => {
         <div className='countries'>
             <ul className="radio-container">
                 <input type="range" min="1" max="250" defaultValue={rangeValue}
-                onChange={(e) => setRangeValue(e.target.value)}
+                    onChange={(e) => setRangeValue(e.target.value)}
                 />
+                {radios.map((continent) => (
+                    <li>
+                        <input type="radio" id={continent} name="continentRadio" 
+                        onChange={(e) => setSelectedRadio(e.target.id)}/>
+                        
+                        <label htmlFor={continent}>{continent}</label>
+                    </li>
+
+                ))}
             </ul>
             <ul>
-                {data.slice(0, rangeValue).map((country, index) => ( 
-                <Card key={index} country={country}/>
+                {data
+                .filter((country) => country.continents[0].includes(selectedRadio))
+                .slice(0, rangeValue)
+                .map((country, index) => (
+                    <Card key={index} country={country} />
                 ))}
             </ul>
         </div>
